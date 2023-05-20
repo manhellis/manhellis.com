5abd694d8c71cf4dddfd3f361ea520ef290f8096
manhellis <manh@manhellis.com>
2023-05-20 01:39:46 -0700

    working nav ugly

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -1,8 +1,13 @@
 const menuToggle = document.getElementById('menu-toggle');
 const sideNav = document.getElementById('side-nav');
+const closeNavButton = document.getElementById('close-nav');
 
 menuToggle.addEventListener('click', () => {
   sideNav.classList.toggle('show');
   menuToggle.textContent = sideNav.classList.contains('show') ? '✕' : '☰';
 });
 
+closeNavButton.addEventListener('click', () => {
+  sideNav.classList.remove('show');
+  menuToggle.textContent = '☰';
+});
